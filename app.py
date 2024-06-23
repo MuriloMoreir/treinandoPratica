5b989f435c3b15cc0bbeb7c4ba909a38b95fee36
MuriloMoreir <124629928+MuriloMoreir@users.noreply.github.com>
2024-06-23 18:54:47 -0300

    Update app.py

--- app.py
+++ app.py
@@ -3,11 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def devops():
- return '<center><h1><font color=red>Germinare Tech, EU AMO DEVOPS</center>'
-
-@app.route('/Murilo')
-def murilo():
- return '<center><h1><font color=red>Germinare Tech, EU AMO DEVOPS, MURILO GST</center>'
-
+ return '<center><h1><font color=red>Germinare Tech, EU AMO DEVOPS. MUIRLO EU TE AMOOOO GSTTTT</center>'
+ 
 if __name__ == '__main__':
  app.run(debug=True, host='0.0.0.0')
